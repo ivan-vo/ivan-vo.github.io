e88132df425e673a1be852b73874519d70a54a72
Ivan Voronov <ivanvo@tahometer.com>
2021-04-21 13:07:54 +0300

    Добавлена можливість відправки форми

--- items/scripts/script-item.js
+++ items/scripts/script-item.js
@@ -1,45 +1,67 @@
 console.log("Script was connected");
 
 class Item {
-    constructor(id, title, dueDate, done, description) {
+    constructor(title, dueDate, description, done) {
+        if (typeof title === 'object') {
+            Object.assign(this, title);
+        }else{
         this.title = title;
         dueDate === '' ? this.dueDate = undefined : this.dueDate = dueDate;;
         this.description = description;
-        done === undefined ? this.done = false : this.done = done;
-        this.id = id;
+        done === undefined ? this.done = false : this.done = done;        
+        this.id = parseInt(new Date().getTime() + "" + Math.floor(Math.random() * 1000));
+        }
     }
 }
 
 let tasks = [];
-let milk = new Item(1, 'Buy milk', new Date(2021, 3, 20), true, "Buy milk, 400ml, 2,5%, korivka");
+let milk = new Item('Buy milk', new Date(2021, 3, 20), "Buy milk, 400ml, 2,5%, korivka", true);
 
 tasks.push(milk);
-tasks.push(new Item(2, 'Buy car', "", false, "Buy new Toyota supra :)"));
-tasks.push(new Item(3, 'Win the math', new Date(2021, 2, 20), false));
-tasks.push(new Item(4, 'Win the math', new Date(2021, 5, 20), false, "win the volleyball match"));
-tasks.push(new Item(5, 'New PC', new Date(2021, 4, 19), false, "Buy new laptop(Lenovo ThinkPad X440) 16:CRM"));
-tasks.push(new Item(6, 'Win the math', new Date(2021, 2, 19), true, "win the futball match"));
+tasks.push(new Item('Buy car', "", "Buy new Toyota supra :)", false));
+tasks.push(new Item('Win the math', new Date(2021, 2, 20), "",false));
+tasks.push(new Item('Win the math', new Date(2021, 5, 20), "win the volleyball match", false));
+tasks.push(new Item('New PC', new Date(2021, 4, 19), "Buy new laptop(Lenovo ThinkPad X440) 16:CRM", false));
+tasks.push(new Item('Win the math', new Date(2021, 2, 19), "win the futball match", true));
 
 const sectionTask = document.getElementById('tasks');
 getAllTask()
+
+const taskForm = document.forms['taskForm'];
+taskForm.addEventListener('submit', (event) => {
+    console.log("Submitted");
+    event.preventDefault();
+    const formItemData = new FormData(taskForm);
+    const objectItem = Object.fromEntries(formItemData.entries());
+    if (!(objectItem.dueDate === '')) {
+        objectItem.dueDate = new Date(objectItem.dueDate);
+    }
+    const item = new Item(objectItem);
+    tasks.push(item);
+    appendItem(item);
+    taskForm.reset();
+}) 
+
+
 
 function clearSection() {
     sectionTask.innerHTML = "";
 }
 
 function getAllTask() {
+    document.getElementById("task-mod-1").checked = true;
     clearSection();
     tasks.forEach(appendItem);
 }
 
 function getAllNotDoneTask() {
+    document.getElementById("task-mod-2").checked = true;
     clearSection();
     tasks.forEach(function(item, index, array) {
         if (item.done === false) {
             appendItem(item);
         }
     });
-    notDoneTasks.forEach(appendItem);
 }
 
 function removeItem(_target, _section) {
@@ -64,6 +86,12 @@ function SetDoneByCheckBox(_event) {
     tasks.forEach(function(item, index, array) {
         if (item.id == sectionId) {
             _event.target.checked ? item.done = true : item.done = false;
+            if (document.getElementById("task-mod-2").checked) {
+                getAllNotDoneTask();
+            }
+            else{
+                getAllTask()
+            }
         }
     });
 }
@@ -77,7 +105,7 @@ function appendItem(item) {
     }
     result += ` <span class = "title"> ${item.title } </span>`;
     let date;
-    if (item.dueDate === undefined) {
+    if (item.dueDate === undefined || item.dueDate ==="") {
         date = null;
     } else {
         date = item.dueDate.toString().split(' ');
